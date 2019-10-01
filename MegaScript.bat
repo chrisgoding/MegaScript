d72a86fd50c3f8945080cae48dd8cfbc94e5a91e
chrisgoding <55512954+chrisgoding@users.noreply.github.com>
2019-10-01 09:14:43 -0400

    Update MegaScript.bat

--- MegaScript.bat
+++ MegaScript.bat
@@ -1,3 +1,8 @@
+setlocal enabledelayedexpansion
+echo %cd% | find "KACE"
+if %errorlevel%==0 ( set KACERUN=True ) else (set KACERUN=False )
+
+if %KACERUN%==True goto start
 ::::::::::::::::::::::::::::::::::::::::::::
 :: Elevate.cmd - Version 4
 :: Automatically check & get admin rights
@@ -53,35 +58,9 @@
  setlocal & cd /d %~dp0
  if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
 
- ::::::::::::::::::::::::::::
- ::START
- ::::::::::::::::::::::::::::
-
-:::::::::::::::::
-:: MEGASCRIPT.BAT
-:::::::::::::::::
-
-pushd "%~dp0"
-setlocal enabledelayedexpansion
-IF EXIST "%SystemRoot%\Sysnative\msiexec.exe" (set "SystemPath=%SystemRoot%\Sysnative") ELSE (set "SystemPath=%SystemRoot%\System32")
-set "path=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SystemRoot%\System32\WindowsPowerShell\v1.0\"
-
-set logdirectory=\\yourservername\MegaScript\Logs
-
-:onbattery
-WMIC Path Win32_Battery Get BatteryStatus | find "1"
-if %errorlevel%==0 @echo off && cls && echo Running on battery power. Please plug in a power adapter when running the megascript. && timeout 30 >nul && goto eof
-
-:startscript
-	if not exist "C:\IT Folder\Megascript Progress Report" mkdir "C:\IT Folder\Megascript Progress Report" >nul
-	if not exist "%logdirectory%\%computername%" mkdir "%logdirectory%\%computername%" >nul
-
-set today=!date:/=-!
-set now=!time::=-!
-set millis=!now:*.=!
-set now=!now:.%millis%=!
-echo %username% > "C:\IT Folder\Megascript progress report\!today!_!now! MegaScript initiated by %username%.txt
-
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+explorer "C:\IT Folder\MegaScript Progress Report"
+:start
 @echo off
 cls
 echo.
@@ -93,128 +72,250 @@ echo Most errors in this are safe to ignore, but if you're concerned, take a
 echo screenshot and email it to chrisgoding@polk-county.net, along with your PC 
 echo name, so that I can find the log file.
 echo _______________________________________________________________________________
-@echo on
 
-%SystemRoot%\explorer.exe "c:\IT Folder\MegaScript Progress Report"
+pushd "%~dp0"
+setlocal enabledelayedexpansion
+IF EXIST "%SystemRoot%\Sysnative\msiexec.exe" (set "SystemPath=%SystemRoot%\Sysnative") ELSE (set "SystemPath=%SystemRoot%\System32")
+set "path=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SystemRoot%\System32\WindowsPowerShell\v1.0\"
 
-:phase0
-	set today=!date:/=-!
-	set now=!time::=-!
-	set millis=!now:*.=!
-	set now=!now:.%millis%=!
-	Echo Starting Phase 0
-	call stuff\phase0\phase0.bat > "C:\it folder\megascript progress report\!today!_!now! phase 0.txt"
-	Echo Phase 0 Complete
-	if exist "%logdirectory%\%computername%" ( "%SystemPath%\robocopy.exe" "C:\it folder\megascript progress report" "%logdirectory%\%computername%" /mir >nul )
-	popd
+set logdirectory=\\SERVER\MEGASCRIPTSHARE\Logs
+if not exist "C:\IT Folder\Megascript Progress Report" mkdir "C:\IT Folder\Megascript Progress Report" >nul
+if not exist "%logdirectory%\%computername%" mkdir "%logdirectory%\%computername%" >nul
 
-:uninstallstuff
-	set today=!date:/=-!
-	set now=!time::=-!
-	set millis=!now:*.=!
-	set now=!now:.%millis%=!
-	pushd "%~dp0"
-	Echo Starting Phase 1 - Bad Software Remover
-	call stuff\phase1\uninstaller.bat > "C:\it folder\megascript progress report\!today!_!now! phase 1.txt" &:: Removing bad software
-	Echo Phase 1 Complete
-	if exist "%logdirectory%\%computername%" ( "%SystemPath%\robocopy.exe" "C:\it folder\megascript progress report" "%logdirectory%\%computername%" /mir >nul )
-	popd
 
-:updatesoftware
-	set today=!date:/=-!
-	set now=!time::=-!
-	set millis=!now:*.=!
-	set now=!now:.%millis%=!
-	pushd "%~dp0"
-	Echo Starting Phase 2 - Software Updater
-	Echo "Access denied" errors are normal when flash is being updated and are safe to ignore.
-	call stuff\phase2\softwareupdater.bat > "C:\it folder\megascript progress report\!today!_!now! phase 2.txt"
-	Echo Phase 2 Complete
-	if exist "%logdirectory%\%computername%" ( "%SystemPath%\robocopy.exe" "C:\it folder\megascript progress report" "%logdirectory%\%computername%" /mir >nul )
-	popd
+if %KACERUN%==True goto ModeSelectKace
+if %KACERUN%==False goto ModeSelectManual
 
-:updateoffice
-	set today=!date:/=-!
-	set now=!time::=-!
-	set millis=!now:*.=!
-	set now=!now:.%millis%=!
-	pushd "%~dp0"
-	Echo Starting Phase 2.5 Office Upgrader
-	call stuff\phase2.5\Install.bat > "C:\it folder\megascript progress report\!today!_!now! phase 2.5.txt"
-	Echo Phase 2.5 Complete
-	if exist "%logdirectory%\%computername%" ( "%SystemPath%\robocopy.exe" "C:\it folder\megascript progress report" "%logdirectory%\%computername%" /mir >nul )
-	popd
+:ModeSelectKace
+	IF %1==/Pico goto RunPico
+	IF %1==/Mini goto RunMini
+	IF %1==/Mega goto RunKaceMega
+	IF %1==/Ultra goto RunKaceUltra
 
-:inplaceupgrade &:: checks if you are running windows 10, then if you are, checks if you are running 1803 or 1809. if older builds are found, initiates in-place upgrade.
-	set today=!date:/=-!
-	set now=!time::=-!
-	set millis=!now:*.=!
-	set now=!now:.%millis%=!
-	pushd "%~dp0"
-	Echo Starting Phase 3 - Windows Upgrade
-	call stuff\Phase3\phase3no7210.bat > "C:\it folder\megascript progress report\!today!_!now! phase 3.txt"
-	Echo Phase 3 Complete
-	if exist "%logdirectory%\%computername%" ( "%SystemPath%\robocopy.exe" "C:\it folder\megascript progress report" "%logdirectory%\%computername%" /mir >nul )
-	popd
+:ModeSelectManual
+	IF %1==/Pico goto RunPico
+	IF %1==/Mini goto RunMini
+	IF %1==/Mega goto RunManualMega
+	IF %1==/Ultra goto RunManualUltra
 
-:windowsupdate
-	set today=!date:/=-!
-	set now=!time::=-!
-	set millis=!now:*.=!
-	set now=!now:.%millis%=!
-	pushd "%~dp0"
-	Echo Starting Phase 4 - Windows Updates
-	call stuff\phase4\wsus\cmd\doupdate.cmd /instdotnet4 /instwmf /updatetsc /updatercerts /updatecpp /monitoron > "C:\it folder\megascript progress report\!today!_!now! phase 4.txt"
-	Echo Phase 4 Complete
-	if exist "%logdirectory%\%computername%" ( "%SystemPath%\robocopy.exe" "C:\it folder\megascript progress report" "%logdirectory%\%computername%" /mir >nul )
-	popd
-
-:win10changes &:: checks if windows 10 is installed, and runs the decrapifier if it is.
-	set today=!date:/=-!
-	set now=!time::=-!
-	set millis=!now:*.=!
-	set now=!now:.%millis%=!
-	pushd "%~dp0"
-	Echo Starting Phase 5 - Win 10 Changes
-	call stuff\phase5\changes.bat > "C:\it folder\megascript progress report\!today!_!now! phase 5.txt"
-	Echo Phase 5 Complete
-	if exist "%logdirectory%\%computername%" ( "%SystemPath%\robocopy.exe" "C:\it folder\megascript progress report" "%logdirectory%\%computername%" /mir >nul )
-	popd
-
-:cleanup &:: runs a rather thorough cleanup. this step could take a long time
-	set today=!date:/=-!
-	set now=!time::=-!
-	set millis=!now:*.=!
-	set now=!now:.%millis%=!
-	pushd "%~dp0"
-	Echo Starting Phase 6 - Cleanup
-	call stuff\phase6\cleanup.bat > "C:\it folder\megascript progress report\!today!_!now! phase 6.txt"
-	Echo Phase 6 Complete
-	if exist "%logdirectory%\%computername%" ( "%SystemPath%\robocopy.exe" "C:\it folder\megascript progress report" "%logdirectory%\%computername%" /mir >nul )
-	popd
-
-:driverupdate &:: calls HP's System Software Manager to upgrade device drivers, software, and firmware
-	set today=!date:/=-!
-	set now=!time::=-!
-	set millis=!now:*.=!
-	set now=!now:.%millis%=!
-	pushd "%~dp0"
-	Echo Starting Phase 7 - Driver Update
-	call stuff\phase7\ssm.bat > "C:\it folder\megascript progress report\!today!_!now! phase 7.txt"
-	Echo Phase 7 Complete
+:RunPico
+	call :GetTime
+	echo %username% > "C:\IT Folder\Megascript progress report\!today!_!now! PicoScript initiated by %username%.txt
+	call :Phase0
+	call :Phase1
+	call :Phase5Quick
+	call :Phase6Quick
+	call :EndPico
 	goto eof
 
-:eof
-	popd
-	pushd %~dp0"
+:RunMini
+	call :GetTime
+	echo %username% > "C:\IT Folder\Megascript progress report\!today!_!now! MiniScript initiated by %username%.txt
+	call :BatteryCheck
+	call :Phase0
+	call :Phase1
+	call :Phase2
+	call :Phase5Quick
+	call :Phase6Quick
+	call :Phase7
+	call :Chrome
+	goto eof
+
+:RunKaceMega
+	call :GetTime
+	echo %username% > "C:\IT Folder\Megascript progress report\!today!_!now! MegaScript initiated by %username%.txt
+	call :BatteryCheck
+	call :Phase0
+	call :Phase1
+	call :Phase2
+	call :Phase2.5
+	call :Phase3No7210
+	call :Phase4Kace
+	call :Phase5Long
+	call :Phase6Ultra
+	call :Phase7
+	call :Chrome
+	goto eof
+
+:RunManualMega
+	call :GetTime
+	echo %username% > "C:\IT Folder\Megascript progress report\!today!_!now! MegaScript initiated by %username%.txt
+	call :BatteryCheck
+	call :Phase0
+	call :Phase1
+	call :Phase2
+	call :Phase2.5
+	call :Phase3No7210
+	call :Phase4Manual
+	call :Phase5Long
+	call :Phase6Medium
+	call :Phase7
+	call :Chrome
+	goto eof
+
+:RunKaceUltra
+	call :GetTime
+	echo %username% > "C:\IT Folder\Megascript progress report\!today!_!now! UltraScript initiated by %username%.txt
+	call :BatteryCheck
+	call :Phase0
+	call :Phase1
+	call :Phase2
+	call :Phase2.5
+	call :Phase3KaceUltra
+	call :Phase4Kace
+	call :Phase5Long
+	call :Phase6Ultra
+	call :Phase7
+	call :Chrome
+	goto eof
+
+:RunManualUltra
+	call :GetTime
+	echo %username% > "C:\IT Folder\Megascript progress report\!today!_!now! UltraScript initiated by %username%.txt
+	call :BatteryCheck
+	call :Phase0
+	call :Phase1
+	call :Phase2
+	call :Phase2.5
+	call :Phase3ManualUltra
+	call :Phase4Manual
+	call :Phase5Long
+	call :Phase6Ultra
+	call :Phase7
+	call :Chrome
+	goto eof
+
+:BatteryCheck
+	WMIC Path Win32_Battery Get BatteryStatus | find "1"
+	if %errorlevel%==0 @echo off && cls && echo Running on battery power. Please plug in a power adapter when running the megascript. && timeout 30 >nul && goto eof
+	exit /b
+
+:Phase0
+	call :StartPhase
+	call stuff\phase0\phase0.bat > "C:\it folder\megascript progress report\!today!_!now! phase 0.txt"
+	call :EndPhase
+	exit /b
+
+:Phase1
+	call :StartPhase
+	call stuff\phase1\uninstaller.bat > "C:\it folder\megascript progress report\!today!_!now! phase 1.txt"
+	call :EndPhase
+	exit /b
+
+:Phase2
+	call :StartPhase
+	Echo "Access denied" errors are normal when flash is being updated and are safe to ignore.
+	call stuff\phase2\softwareupdater.bat > "C:\it folder\megascript progress report\!today!_!now! phase 2.txt"
+	call :EndPhase
+	exit /b
+
+:Phase2.5
+	call :StartPhase
+	call stuff\phase2.5\Install.bat > "C:\it folder\megascript progress report\!today!_!now! phase 2.5.txt"
+	call :EndPhase
+	exit /b
+
+:Phase3No7210
+	call :StartPhase
+	call stuff\Phase3\phase3no7210.bat > "C:\it folder\megascript progress report\!today!_!now! phase 3.txt"
+	call :EndPhase
+	exit /b
+
+:Phase3KaceUltra
+	call :StartPhase
+	call stuff\Phase3\phase34kace.bat > "C:\it folder\megascript progress report\!today!_!now! phase 3.txt"
+	call :EndPhase
+	exit /b
+
+:Phase3ManualUltra
+	call :StartPhase
+	call stuff\Phase3\phase3.bat > "C:\it folder\megascript progress report\!today!_!now! phase 3.txt"
+	call :EndPhase
+	exit /b
+
+:Phase4Kace
+	call :StartPhase
+	call stuff\phase4\wsus\cmd\doupdate.cmd /instdotnet4 /instwmf /updatercerts /monitoron > "C:\it folder\megascript progress report\!today!_!now! phase 4.txt"
+	call :EndPhase
+	exit /b
+
+:Phase4Manual
+	call :StartPhase
+	call stuff\phase4\wsus\cmd\doupdate.cmd /instdotnet4 /instwmf /updatetsc /updatercerts /updatecpp /monitoron > "C:\it folder\megascript progress report\!today!_!now! phase 4.txt"
+	call :EndPhase
+	exit /b
+
+:Phase5Quick
+	call :StartPhase
+	call stuff\phase5\quickchanges.bat > "C:\it folder\megascript progress report\!today!_!now! phase 5.txt"
+	call :EndPhase
+	exit /b
+
+:Phase5Long
+	call :StartPhase
+	call stuff\phase5\changes.bat > "C:\it folder\megascript progress report\!today!_!now! phase 5.txt"
+	call :EndPhase
+	exit /b
+
+:Phase6Quick
+	call :StartPhase
+	call stuff\phase6\quickcleanup.bat > "C:\it folder\megascript progress report\!today!_!now! phase 6.txt"
+	call :EndPhase
+	exit /b
+
+:Phase6Medium
+	call :StartPhase
+	call stuff\phase6\cleanup.bat > "C:\it folder\megascript progress report\!today!_!now! phase 6.txt"
+	call :EndPhase
+	exit /b
+
+:Phase6Ultra
+	call :StartPhase
+	call stuff\phase6\ultracleanup.bat > "C:\it folder\megascript progress report\!today!_!now! phase 6.txt"
+	call :EndPhase
+	exit /b
+
+:EndPico
+	call :StartPhase
+	pushd stuff\phase7
+	call disablewifi.bat
+	call postssm.bat > "C:\it folder\megascript progress report\!today!_!now! phase 7.txt"
+	call :EndPhase
+	exit /b
+
+:Phase7
+	call :StartPhase
+	call stuff\phase7\ssm.bat > "C:\it folder\megascript progress report\!today!_!now! phase 7.txt"
+	call :EndPhase
+	exit /b
+
+:Chrome
+	call :StartPhase
 	call stuff\phase2\chrome.bat
+	"%SystemPath%\shutdown.exe" -a
+	"%SystemPath%\shutdown.exe" -r -t 10 -f
+	call :EndPhase
+	exit /b
+
+:StartPhase
+	call :GetTime
+	pushd "%~dp0"
+	exit /b
+
+:EndPhase
+	if exist "%logdirectory%\%computername%" ( "%SystemPath%\robocopy.exe" "C:\it folder\megascript progress report" "%logdirectory%\%computername%" /mir >nul )
+	popd
+	exit /b
+
+:GetTime
 	set today=!date:/=-!
 	set now=!time::=-!
 	set millis=!now:*.=!
 	set now=!now:.%millis%=!
+	exit /b
+
+:eof
+	call :GetTime!
 	Echo Done! > "C:\it folder\megascript progress report\!today!_!now! script run complete.txt"
-	if exist "%logdirectory%\%computername%" ( "%SystemPath%\robocopy.exe" "C:\it folder\megascript progress report" "%logdirectory%\%computername%" /mir >nul )
-	"%SystemPath%\shutdown.exe" -a
-	"%SystemPath%\shutdown.exe" -r -t 120 -f
+	call :EndPhase
 	endlocal
-	popd
