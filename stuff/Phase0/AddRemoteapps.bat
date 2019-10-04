646b9bc62f95a4f198598e013f356763bbf63b12
chrisgoding <55512954+chrisgoding@users.noreply.github.com>
2019-10-04 16:15:39 -0400

    Update AddRemoteapps.bat

--- stuff/Phase0/AddRemoteapps.bat
+++ stuff/Phase0/AddRemoteapps.bat
@@ -11,10 +11,10 @@
 :: email chrisgoding@polk-county.net with any
 :: concerns or suggestions
 :::::::::::::::::::::::::::::::::::::::::::::::::
-echo %userdomain% | find "POLK"
+echo %userdomain% | find "<CHANGE THIS TO YOUR USERDOMAIN>"
 if %errorlevel%==1 ( goto eof )
-if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Polk County (RADC)" goto eof
-if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\RemoteApp and Desktop Connections\Polk County" goto eof
+if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\<Change this to the name of the folder that appears in your windows 10 start menu after installing your .wcx file>" goto eof
+if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\RemoteApp and Desktop Connections\<Change this to the name of the folder that appears in you windows 7 start menu after installing your .wcx file>" goto eof
 
 PowerShell.exe -ExecutionPolicy Bypass -File "C:\IT Folder\Area 51\programs\Install-RADCConnection.PS1"
 :eof
