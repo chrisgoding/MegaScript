7e34aad412597c9bc4228a2ffbdbe0cdf0fd7bf6
chrisgoding <55512954+chrisgoding@users.noreply.github.com>
2020-02-17 11:36:58 -0500

    Update update replication shares.bat
    changed the way the repshares.txt works, so now the omegascript can read it too

--- update replication shares.bat
+++ update replication shares.bat
@@ -3,33 +3,33 @@ setlocal enabledelayedexpansion
 IF EXIST "%SystemRoot%\Sysnative\msiexec.exe" (set "SystemPath=%SystemRoot%\Sysnative") ELSE (set "SystemPath=%SystemRoot%\System32")
 set "path=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SystemRoot%\System32\WindowsPowerShell\v1.0\"
 
-Call :CleanHPIADriverStore
 Call :ReplicateMegaScript
+Call :CleanHPIADriverStore
+
 Goto EOF
 
 :::::::::::::
 ::FUNCTIONS::
 :::::::::::::
 
+:ReplicateMegaScript
+	for /F "tokens=2" %%B in ( RepShares.txt ) do ( 
+	if exist %%B ( echo Replicating...&& ROBOCOPY "\\vboccfs02.polk-county.net\IT_SSM" "%%B" /MIR /XD "\\vboccfs02.polk-county.net\IT_SSM\Logs" /XD "\\vboccfs02.polk-county.net\it_ssm\stuff\Phase7\HPIA\Drivers" XD "\\vboccfs02.polk-county.net\it_ssm\stuff\Phase7\Dell" /COPY:DT >nul ) else ( echo %%B unreachable )
+	)
+	exit /b
+
 :CleanHPIADriverStore
-	for /F "tokens=*" %%A in ( RepShares.txt ) do ( 
+	for /F "tokens=2" %%A in ( RepShares.txt ) do (
+	if exist %%A ( 
 	pushd %%A
 	ForFiles /p "%CD%stuff\Phase7\HPIA\Drivers" /s /d -180 /c "cmd /c del @file"
 	ForFiles /p "%CD%stuff\Phase7\HPIA\Drivers" -d -180 -c "cmd /c IF @isdir == TRUE rd /S /Q @path"
 	popd
+	) else ( echo %%A unreachable )
 	)
-	pushd \\<YOUR SERVER>\<YOUR SHARE>
+	pushd \\vboccfs02.polk-county.net\it_ssm
 	ForFiles /p "%CD%stuff\Phase7\HPIA\Drivers" /s /d -180 /c "cmd /c del @file"
 	ForFiles /p "%CD%stuff\Phase7\HPIA\Drivers" -d -180 -c "cmd /c IF @isdir == TRUE rd /S /Q @path"
-	pause
-	exit /b
-
-
-:ReplicateMegaScript
-	for /F "tokens=*" %%B in ( RepShares.txt ) do ( 
-	if exist %%B ( echo Replicating...&& ROBOCOPY "\\<YOUR SERVER>\<YOUR SHARE>" "%%B" /MIR /XD "\\<YOUR SERVER>\<YOUR SHARE>\Logs" /XD "\\<YOUR SERVER>\<YOUR SHARE>\stuff\Phase7\HPIA\Drivers" XD "\\<YOUR SERVER>\<YOUR SHARE>\stuff\Phase7\Dell" /COPY:DT >nul ) else ( echo Share unreachable )
-	)
-@pause
 	exit /b
 
 :EOF
