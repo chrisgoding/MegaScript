8010ee162868ac41678e3d965ec667e965b75ab8
chrisgoding <55512954+chrisgoding@users.noreply.github.com>
2020-02-17 12:41:37 -0500

    Update install.bat

--- stuff/Phase2/acroreader/install.bat
+++ stuff/Phase2/acroreader/install.bat
@@ -5,6 +5,29 @@ set "path=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SystemR
 @echo on
 
 del *.log /F /Q
+set install=true
+call :closeprograms
+call :removereader
+call :installreader
+goto eof
+
+:::::::::::::
+::FUNCTIONS::
+:::::::::::::
+
+
+:abort
+	"%SystemPath%\tasklist.exe" /FI "imagename eq outlook.exe"|"%SystemPath%\find.exe" /I "outlook.exe">NUL
+	if %errorlevel% == 0 set install=false&&exit /b
+	"%SystemPath%\tasklist.exe" /FI "imagename eq chrome.exe"|"%SystemPath%\find.exe" /I "chrome.exe">NUL
+	if %errorlevel% == 0 set install=false&&exit /b
+	"%SystemPath%\tasklist.exe" /FI "imagename eq iexplore.exe"|"%SystemPath%\find.exe" /I "iexplore.exe">NUL
+	if %errorlevel% == 0 set install=false&&exit /b
+	"%SystemPath%\tasklist.exe" /FI "imagename eq excel.exe"|"%SystemPath%\find.exe" /I "excel.exe">NUL
+	if %errorlevel% == 0 set install=false&&exit /b
+	"%SystemPath%\tasklist.exe" /FI "imagename eq AcroRd32.exe"|"%SystemPath%\find.exe" /I "AcroRd32.exe">NUL
+	if %errorlevel% == 0 set install=false&&exit /b
+	exit /b
 
 :closeprograms &:: closes software that may interfere with the installation
 	"%SystemPath%\tasklist.exe" /FI "imagename eq outlook.exe"|"%SystemPath%\find.exe" /I "outlook.exe"
@@ -17,13 +40,29 @@ del *.log /F /Q
 	if %errorlevel%==0 "%SystemPath%\taskkill.exe" /f /im excel.exe >nul
 	"%SystemPath%\tasklist.exe" /FI "imagename eq AcroRd32.exe"|"%SystemPath%\find.exe" /I "AcroRd32.exe"
 	if %errorlevel%==0 "%SystemPath%\taskkill.exe" /f /im AcroRd32.exe >nul
+	exit /b
 
 :removereader
+	if %install%==false exit /b
 	"%SystemPath%\cscript.exe" "RM_Reader.vbs"
 	timeout 5 >nul
-:installreader
-	setup.exe -s
+	exit /b
 
-:end
+:installreader
+	if %install%==false exit /b
+	setup.exe -s
+	exit /b
+
+:eof
 	endlocal
 	popd
+
+
+
+
+
+
+
+
+
+
